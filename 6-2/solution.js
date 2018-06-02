f47ed346b6ebba6f96d5c6c5fada39874132ca96
trevorjdobson <31601148+trevorjdobson@users.noreply.github.com>
2018-06-02 11:44:38 -0700

    Update solution.js

--- 6-2/solution.js
+++ 6-2/solution.js
@@ -1,33 +1,27 @@
 function closest(objArr, k, locObj){
-  let disArr = [];
   
   for(let i =0; i < objArr.length; i++){
     let dist = Math.sqrt((objArr[i].x - locObj.x)^2 + (objArr[i].y - locObj.y)^2);
-    console.log(dist)
-    disArr.push(dist)
     objArr[i].distance = dist
+    objArr[i].tally = 0;
   }
   
-  objArr.sort((a, b) => a-b);
-  console.log('distances', disArr)
+  objArr.sort((a, b) => a.distance-b.distance);
+  console.log('distances', objArr)
   let closest = [];
   
   for(let j=0; j < k; j++){
-    for(let n =0; n<objArr.length; n++){
-      if(objArr[n].distance === disArr[j]){
-        closest.push(objArr[n])
-      }
-    }
+    closest.push(objArr[j]);
   }
-  console.log('closest obj', closest)
-  for(let x=0; x < closest.length -1; x++){
-    closest[x].tally = 0;
+
+  for(let x=0; x < closest.length; x++){
     for(let z=0; z<closest.length; z++){
       if(closest[x].label === closest[z].label){
         closest[x].tally++;
       }
     }
   }
+  console.log('closest obj', closest)
   
   let majority = closest[0];
   
